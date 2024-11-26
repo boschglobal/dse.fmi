5aad8898676505465a25db93eb7d1861b0829d23
Timothy Rule (VM/EMT3) <Timothy.Rule@de.bosch.com>
2024-11-26 12:26:46 +0100

    Fix windows build.
    Signed-off-by: Timothy Rule (VM/EMT3) <Timothy.Rule@de.bosch.com>

--- dse/importer/importer.h
+++ dse/importer/importer.h
@@ -6,8 +6,12 @@
 #define DSE_IMPORTER_IMPORTER_H_
 
 
-#define DLL_PUBLIC  __attribute__((visibility("default")))
+#ifndef DLL_PUBLIC
+#define DLL_PUBLIC __attribute__((visibility("default")))
+#endif
+#ifndef DLL_PRIVATE
 #define DLL_PRIVATE __attribute__((visibility("hidden")))
+#endif
 
 
 /**
